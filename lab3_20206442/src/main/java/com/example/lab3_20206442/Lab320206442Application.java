edce13a8e0e02c881c76c09d5044704d9bcfeb12
joselin <a20206442@pucp.edu.pe>
2023-08-31 20:42:46 -0500

    archivo base

---
+++ lab3_20206442/src/main/java/com/example/lab3_20206442/Lab320206442Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20206442;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320206442Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320206442Application.class, args);
+    }
+
+}
